43c95d29eee0e3ee03c0db71b33456f8fd86b4bf
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2024-07-02 15:55:11 +0530

    Time: 113 ms (20.31%) | Memory: 54.5 MB (69.96%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -7,43 +7,44 @@ var findMedianSortedArrays = function(nums1, nums2) {
     let N = nums1.length,  // Length of the first array
       M = nums2.length;  // Length of the second array
 
-  let left = 0;  // Pointer for the first array
-  let right = 0; // Pointer for the second array
+    if(N > M) return findMedianSortedArrays(nums2, nums1)
 
-  let totalLength = M + N;  // Total length of the merged arrays
+    let totalLength = M + N  
+    let noOfLeftElements = Math.floor((totalLength + 1) / 2)   
 
-  let mergedArr = [];  // Array to hold the merged result
+    let low = 0 
+    let high = N
 
-  // Merge the two sorted arrays
-  while (left < N && right < M) {
-    if (nums1[left] <= nums2[right]) {
-      mergedArr.push(nums1[left++]);  // Add element from the first array and increment pointer
-    } else {
-      mergedArr.push(nums2[right++]); // Add element from the second array and increment pointer
+    while(low <= high) {
+        let mid1 = Math.floor((low + high) / 2)
+        let mid2 = noOfLeftElements - mid1
+
+        let l1 = -Infinity
+        let l2 = -Infinity
+        let r1 = Infinity
+        let r2 = Infinity
+        
+        if(mid1 - 1 >= 0) l1 = nums1[mid1 - 1]
+        if(mid2 - 1 >= 0) l2 = nums2[mid2 - 1]
+        if(mid1 < N) r1 = nums1[mid1]
+        if(mid2 < M) r2 = nums2[mid2]
+
+        if(l1 <= r2 && l2 <= r1) {
+            if(totalLength % 2 === 1) {
+                const median = Math.max(l1, l2)
+
+                return median
+            } else {
+                const median = Math.max(l1, l2) + Math.min(r1,r2)
+
+                return median / 2
+            }
+        } else if (l1 > r2) {
+            high = mid1 - 1
+        } else {
+            low = mid1 + 1
+        }
     }
-  }
 
-  // If there are remaining elements in the first array, add them
-  while (left < N) {
-    mergedArr.push(nums1[left++]);
-  }
-
-  // If there are remaining elements in the second array, add them
-  while (right < M) {
-    mergedArr.push(nums2[right++]);
-  }
-
-  // Find the median based on the total length
-  if (totalLength % 2 == 1) {
-    const median = Math.floor(totalLength / 2);  // If the total length is odd, get the middle element
-    return mergedArr[median];
-  }
-
-  // If the total length is even, calculate the average of the two middle elements
-  const medianIndexOne = totalLength / 2;
-  const medianIndexTwo = totalLength / 2 - 1;
-
-  const median = (mergedArr[medianIndexOne] + mergedArr[medianIndexTwo]) / 2;
-
-  return median;
+    return 0
 };
