28d32956992b373e16d825f09511358c0fe0f37f
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2025-08-14 21:12:12 +0530

    Time: 3 ms (46.00%) | Memory: 58 MB (69.38%) - LeetSync

---
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if (!root) return []
+
+    let result = []
+    let queue = [root]
+
+    while(queue.length) {
+        let levelSize = queue.length
+        let level = []
+
+        for(let i = 0; i < levelSize; i++) {
+            let currentNode = queue.shift()
+            level.push(currentNode.val)
+
+            if(currentNode.left) queue.push(currentNode.left)
+            if(currentNode.right) queue.push(currentNode.right)
+        }
+
+        result.push(level)
+    }
+
+    return result
+};
