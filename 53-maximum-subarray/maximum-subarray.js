7f4592d5719eee402f5ba7e8af0d3c40ffcfea73
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2025-08-26 23:09:16 +0530

    Time: 0 ms (100.00%) | Memory: 63.4 MB (73.79%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -2,55 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-
-//  Kadane's Algo
 var maxSubArray = function(nums) {
-    let currentMax = nums[0]
-    let globalMax = nums[0]
+    let bestSum = nums[0]
+    let currentSum = nums[0]
 
-    for(let i = 1; i < nums.length; i++){
-        currentMax = Math.max(nums[i], currentMax + nums[i])
+    for(let i = 1; i < nums.length; i++) {
+        const val = nums[i]
 
-        if(currentMax > globalMax) {
-            globalMax = currentMax
-        }
+        currentSum = Math.max(val, currentSum + val)
+
+        if(currentSum > bestSum) bestSum = currentSum
     }
 
-
-    return globalMax
+    return bestSum
 };
-
-// Brute Force Approach 
-
-function maxSubArray(nums) {
-    let maxSum = Number.NEGATIVE_INFINITY;
-
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i; j < nums.length; j++) {
-            let currentSum = 0;
-            for (let k = i; k <= j; k++) {
-                currentSum += nums[k];
-            }
-
-            maxSum = Math.max(maxSum, currentSum);
-        }
-    }
-
-    return maxSum;
-}
-
-// Better Approach 
-
-function maxSubArray(nums) {
-    let maxSum = Number.NEGATIVE_INFINITY;
-
-    for (let i = 0; i < nums.length; i++) {
-        let currentSum = 0;
-        for (let j = i; j < nums.length; j++) {
-            currentSum += nums[j]; // Add the current element to the sum
-            maxSum = Math.max(maxSum, currentSum); // Update maxSum if currentSum is greater
-        }
-    }
-
-    return maxSum;
-}
