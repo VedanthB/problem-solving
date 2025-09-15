ba42bead561f76b633e095da30ef651b0ca27365
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2025-09-16 04:20:57 +0530

    Time: 1062 ms (25.69%) | Memory: 56.5 MB (97.59%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.js
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.js
@@ -4,18 +4,31 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    let row = 0
-    let column = matrix[0].length - 1
+     const rows = matrix.length
+     const cols = matrix[0].length 
 
-    while(row < matrix.length && column >= 0) {
-        if(matrix[row][column] === target) {
-            return true
-        } else if(matrix[row][column] < target) {
-            row++
-        } else {
-            column--
+     let left = 0
+     let right = cols - 1
+
+     for(let i = 0; i < rows; i++) {
+        const first = matrix[i][0]
+        const last = matrix[i][cols - 1]
+        
+        if(target < first || target > last) continue
+
+        let left = 0, right = cols - 1
+
+        while(left <= right) {
+            const mid = left + Math.floor((right - left) / 2)
+            const value = matrix[i][mid]
+            if(value === target) return true
+            if(value < target) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
         }
-    }
+     }
 
-    return false
+     return false
 };
