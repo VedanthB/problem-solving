8dfdbb351b2ec5879fe00b30608e657094cc2375
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2025-09-17 03:33:46 +0530

    Time: 53 ms (29.71%) | Memory: 57.8 MB (45.01%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    if(!head || !head.next) return null
+
+    let slow = head
+    let fast = head
+
+    while(fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
+
+        if(fast === slow) break
+    }
+
+    if(!fast || !fast.next) return null
+
+    slow = head
+
+    while(slow !== fast) {
+        slow = slow.next
+        fast = fast.next
+    } 
+
+    return fast
+};
