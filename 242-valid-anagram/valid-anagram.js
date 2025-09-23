0416e35aae9b1f22028ad5b80c01d81a9fff2c2e
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2025-09-23 13:30:40 +0530

    Time: 4 ms (97.95%) | Memory: 54.4 MB (98.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -7,11 +7,16 @@ var isAnagram = function(s, t) {
     if(s.length !== t.length) return false
 
     const count = new Array(26).fill(0)
+    const base = 'a'.charCodeAt(0)
 
     for(let i = 0; i < s.length; i++) {
-        count[s.charCodeAt(i) - 97]++ 
-        count[t.charCodeAt(i) - 97]-- 
+        count[s.charCodeAt(i) - base]++
+        count[t.charCodeAt(i) - base]--
     }
 
-    return count.every(val => val === 0)
+    for(let i = 0; i < 26; i++) {
+        if(count[i] !== 0) return false
+    }
+
+    return true
 };
