11ad635e1d24f937d0865a1db833dcf7f586bf90
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2025-09-23 14:02:48 +0530

    Time: 1 ms (87.97%) | Memory: 58.3 MB (7.53%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,69 +3,19 @@
  * @param {number} target
  * @return {number[]}
  */
-
-//  most optimal solution suing two pointer
 var twoSum = function(nums, target) {
-    // Create an array of objects with value and original index
-    let arr = nums.map((value, index) => ({ value, index }));
+    const valuetoindex = new Map()
 
-    // Sort the array based on the values
-    arr.sort((a, b) => a.value - b.value);
+    for(let i = 0; i < nums.length; i++) {
+        const current = nums[i]
+        const needed = target - current
 
-    let left = 0;
-    let right = arr.length - 1;
-
-    while (left < right) {
-        let sum = arr[left].value + arr[right].value;
-
-        if (sum === target) {
-            return [arr[left].index, arr[right].index];
-        } else if (sum < target) {
-            left++;
-        } else {
-            right--;
+        if(valuetoindex.has(needed)) {
+            return [valuetoindex.get(needed), i]
         }
+
+        valuetoindex.set(current, i)
     }
 
-    // If no pair is found
-    return [];
+    return [-1, -1]
 };
-
-
-// better approach using hash map
-
-function twoSum(nums, target) {
-    let hashMap = new Map();
-
-    for (let i = 0; i < nums.length; i++) {
-        let complement = target - nums[i];
-
-        // Check if the complement exists in the hash map
-        if (hashMap.has(complement)) {
-            return [hashMap.get(complement), i];
-        }
-
-        // Store the current element and its index in the hash map
-        hashMap.set(nums[i], i);
-    }
-
-    // If no pair is found
-    return [];
-}
-
-// Brute Force Approach 
-
-function twoSum(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
-        }
-    }
-
-    // If no such pair is found
-    return [];
-}
-
-
