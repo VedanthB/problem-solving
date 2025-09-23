d4f74c129ef91ba1cd4aeb7216c99cda311aba6c
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2025-09-23 09:20:46 +0530

    Time: 28 ms (45.26%) | Memory: 58.9 MB (43.60%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.js
+++ 76-minimum-window-substring/minimum-window-substring.js
@@ -6,46 +6,48 @@
 var minWindow = function(s, t) {
     if(t.length > s.length) return ""
 
-    const requiredCount = new Map()
-    for(const ch of t) {
-        requiredCount.set(ch, (requiredCount.get(ch) || 0) + 1)
+    const need = new Map()
+    for(let ch of t) {
+        need.set(ch, (need.get(ch) || 0) + 1)
     }
-    const requiredUnique = requiredCount.size
+    const required = need.size
 
-    const windowMap = new Map()
-    let formedUnique = 0
+    const have = new Map()
+    let formed = 0
+    let beststart = 0, bestlength = Infinity
 
-    let bestLength = Infinity
-    let bestStartIndex = 0
+    let left = 0
 
-    for(let i = 0, j = 0; j < s.length; j++) {
-        const rightCh = s[j]
+    for(let right = 0; right < s.length; right++){
+        const chR = s[right]
 
-        if(requiredCount.has(rightCh)) {
-            windowMap.set(rightCh, (windowMap.get(rightCh) || 0) + 1)
-            if(windowMap.get(rightCh) === requiredCount.get(rightCh)) {
-                formedUnique++
+        if(need.has(chR)) {
+            have.set(chR, (have.get(chR) || 0) + 1)
+
+            if(have.get(chR) === need.get(chR)) {
+                formed++
             }
         }
 
-        while(formedUnique === requiredUnique && i <= j) {
-            const currLength = j - i + 1
-            if(currLength < bestLength) {
-                bestLength = currLength
-                bestStartIndex = i 
+        while(formed === required && left <= right) {
+            if(right - left + 1 < bestlength) {
+                bestlength = right - left + 1
+                beststart = left
             }
 
-            const leftCh = s[i]
-            if(requiredCount.has(leftCh)) {
-                windowMap.set(leftCh, (windowMap.get(leftCh) || 0) - 1)
-                if(windowMap.get(leftCh) < requiredCount.get(leftCh)) {
-                    formedUnique--
+            const chL = s[left]
+
+            if(need.has(chL)) {
+                have.set(chL, (have.get(chL) || 0) - 1)
+
+                if(have.get(chL) < need.get(chL)) {
+                    formed--
                 }
             }
-            i++
-        } 
-    }
-    
 
-    return bestLength === Infinity ? "" : s.slice(bestStartIndex, bestStartIndex + bestLength) 
+            left++
+        }
+    }
+
+    return bestlength === Infinity ? "" : s.slice(beststart, beststart + bestlength)
 };
