5477c92e00cc10f956b2412969d46dcc6377f27a
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2025-09-30 02:02:40 +0530

    Time: 10 ms (36.09%) | Memory: 61.5 MB (31.31%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.js
+++ 130-surrounded-regions/surrounded-regions.js
@@ -2,52 +2,44 @@
  * @param {character[][]} board
  * @return {void} Do not return anything, modify board in-place instead.
  */
-var solve = function(board) {
-    const rows = board.length 
+var solve = function(grid) {
 
-    if (rows === 0) return;                  // guard empty
+    const rows = grid.length
+    const cols = grid[0].length 
 
-    const cols = board[0].length
-
-    const dirs = [
-        [0, 1],
-        [0, -1],
-        [1, 0],
-        [-1, 0]
-    ]
-
-    const inBounds = (r,c) => r >= 0 && r < rows && c >=0 && c < cols
-
-
-
-    function dfsMark(r, c) {
-        if(!inBounds(r, c) || board[r][c] !== "O") return 
+    const inBounds = (r, c) => r >= 0 && r < rows && c >= 0 && c < cols
+    const dirs = [[-1, 0], [1,0],[0, -1], [0, 1]]
     
-        board[r][c] = "S"
+    const queue = []
 
-        for(const [dr, dc] of dirs) {
-            const nr = dr + r, nc = dc + c
-
-            dfsMark(nr, nc)
-        }
+    for(let c = 0; c < cols; c++) {
+        if(grid[0][c] === "O") { grid[0][c] = 'S'; queue.push([0,c]) }
+        if(grid[rows - 1][c] === "O")  { grid[rows - 1][c] = 'S'; queue.push([rows - 1, c]) }
     }
 
     for(let r = 0; r < rows; r++) {
-        if(board[r][0] === "O") dfsMark(r, 0)
-        if(board[r][cols - 1] === "O") dfsMark(r, cols - 1) 
+        if(grid[r][0] === "O") {grid[r][0] = 'S'; queue.push([r, 0]) }
+        if(grid[r][cols - 1] === "O")  { grid[r][cols - 1] = 'S'; queue.push([r, cols - 1]) }
     }
 
-    for(let c = 0; c < cols; c++) {
-        if(board[0][c] === "O") dfsMark(0, c)
-        if(board[rows -1][c] === "O") dfsMark(rows - 1, c) 
+    while(queue.length) {
+        const [cr, cc] = queue.shift()
+
+        for(const [dr, dc] of dirs) {
+            const nr = dr + cr, nc = dc + cc
+
+            if(inBounds(nr, nc) && grid[nr][nc] === "O") {
+                grid[nr][nc] = "S"
+                queue.push([nr, nc])
+            }
+        }
     }
 
     for(let r = 0; r < rows; r++) {
         for(let c = 0; c < cols; c++) {
-            if(board[r][c] === "S") board[r][c] = "O"
-            else if (board[r][c] === "O") board[r][c] = "X"
+            if(grid[r][c] === 'O') grid[r][c] = 'X'
+            else if(grid[r][c] === 'S') grid[r][c] = 'O'
         }
     }
 
-    return board
 };
