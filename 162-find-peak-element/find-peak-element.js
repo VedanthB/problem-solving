72684a631a7b5c31e8e3783c3334826e89dccdef
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2025-10-21 14:32:47 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (22.04%) - LeetSync

--- 162-find-peak-element/find-peak-element.js
+++ 162-find-peak-element/find-peak-element.js
@@ -3,17 +3,18 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    let left = 0, right = nums.length - 1
+   const n = nums.length 
+   let left = 0
+   let right = n - 1
 
-    while(left < right) {
-        const mid = left + Math.floor((right - left) / 2)
-
-        if(nums[mid] < nums[mid + 1]) {
-            left = mid + 1
-        } else {
-            right = mid
-        }
+   while(left < right) {
+    const mid = left + (( right - left ) >> 1)
+    if(nums[mid] < nums[mid + 1]) {
+        left = mid + 1
+    } else {
+        right = mid
     }
+   }
 
-    return left
+   return left
 };
