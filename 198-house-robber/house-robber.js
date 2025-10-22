be52baea830e45db92d5cb678e3d71069b166e14
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2025-10-22 09:16:18 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (59.15%) - LeetSync

--- 198-house-robber/house-robber.js
+++ 198-house-robber/house-robber.js
@@ -3,21 +3,18 @@
  * @return {number}
  */
 var rob = function(nums) {
-    const n = nums.length
-    if(n === 0) return 0 
-    if(n === 1) return nums[0]
-
     let next = 0
     let next2 = 0
 
-    for(let i = n - 1; i >=0 ; i--) {
-        const take = nums[i] + next2
-        const skip = next
-        const current = Math.max(take, skip)
-        next2 = next
-        next = current
-    } 
 
-    return next
+    for(let amount of nums) {
+        const rob = amount + next
+        const skip = next2
+        const best = Math.max(rob, skip)
 
+        next = next2
+        next2 = best
+    }
+
+    return next2
 };
