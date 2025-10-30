356bd3872d654b74b7851591660575054c534028
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2025-10-30 19:08:09 +0530

    Time: 48 ms (11.90%) | Memory: 61.8 MB (80.95%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.js
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.js
@@ -3,28 +3,34 @@
  * @return {number[]}
  */
 var rowAndMaximumOnes = function(mat) {
-     const numberOfRows = mat.length;
-  const numberOfColumns = mat[0].length;
 
-  let bestRowIndex = 0;
-  let highestOnesCount = 0;
+  function countOnes(row) {
+    let left = 0, right = row.length - 1
 
-  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
-    let onesCountInRow = 0;
-    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
-      if (mat[rowIndex][columnIndex] === 1) {
-        onesCountInRow++;
-      }
+    while(left <= right) {
+        const mid = left + ((right - left) >> 1)
+
+        if(row[mid] == 1) {
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
     }
 
-    if (onesCountInRow > 0 && onesCountInRow > highestOnesCount) {
-      highestOnesCount = onesCountInRow;
-      bestRowIndex = rowIndex;
+    return row.length - left
+  }  
+
+  let result = -1
+  let max = 0
+
+  for(let i = 0; i < mat.length; i++) {
+    let ones = countOnes(mat[i].sort())
+
+    if(ones > max) {
+        max = ones
+        result = i
     }
-    // If equal, we prefer the smaller index, so do nothing
   }
 
-
-
-  return [bestRowIndex, highestOnesCount];
+  return max === 0 ? [0, 0] : [result, max]
 };
