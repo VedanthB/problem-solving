e429b1b340a3040e99a009a1045d5ffa1537c21b
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2025-10-30 19:17:36 +0530

    Time: 256 ms (77.56%) | Memory: 58.5 MB (45.06%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.js
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.js
@@ -4,31 +4,19 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-     const rows = matrix.length
-     const cols = matrix[0].length 
+  const n = matrix.length 
+  const m = matrix[0].length
 
-     let left = 0
-     let right = cols - 1
+  let row = 0
+  let col = m - 1
 
-     for(let i = 0; i < rows; i++) {
-        const first = matrix[i][0]
-        const last = matrix[i][cols - 1]
-        
-        if(target < first || target > last) continue
+  while(row < n && col >=0) {
+    const val = matrix[row][col]
 
-        let left = 0, right = cols - 1
+    if(val === target) return true
+    else if(val > target) col--
+    else row++
+  }  
 
-        while(left <= right) {
-            const mid = left + Math.floor((right - left) / 2)
-            const value = matrix[i][mid]
-            if(value === target) return true
-            if(value < target) {
-                left = mid + 1
-            } else {
-                right = mid - 1
-            }
-        }
-     }
-
-     return false
+  return false
 };
